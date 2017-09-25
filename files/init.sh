3874825e52995e2478947f14c34955b338382484
Mark Lopez <m@silvenga.com>
2017-09-24 23:48:21 -0500

    Added support for passing the host header.

--- files/init.sh
+++ files/init.sh
@@ -3,13 +3,14 @@
 [ -z "${BACKEND_URL}" ] && echo "ERROR: BACKEND_URL must be set (i.e. http://example.com:8080)." && exit 1;
 
 echo "Using backend ${BACKEND_URL}."
-envsubst > /etc/nginx/conf.d/default.conf <<EOF
+cat > /etc/nginx/conf.d/default.conf <<EOF
 server {
-    listen       $PORT;
+    listen       ${PORT};
     server_name  localhost;
     
     location / {
-       proxy_pass $BACKEND_URL;
+       proxy_pass ${BACKEND_URL};
+       proxy_set_header Host \$host;
     }
 }
 EOF
