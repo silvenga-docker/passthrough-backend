5fa5ccdf26ba0e18b7632b7245656d1521106220
Mark Lopez <m@silvenga.com>
2020-08-07 21:30:53 -0500

    Added support for a custom host header.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM nginx:1.13.5-alpine
 
 ENV BACKEND_URL ""
+ENV BACKEND_HOST "$host"
 ENV PORT "80"
 
 COPY files/init.sh /init.sh
--- files/init.sh
+++ files/init.sh
@@ -3,6 +3,7 @@
 [ -z "${BACKEND_URL}" ] && echo "ERROR: BACKEND_URL must be set (i.e. http://example.com:8080)." && exit 1;
 
 echo "Using backend ${BACKEND_URL}."
+echo "Using host ${BACKEND_HOST}."
 
 cat > /etc/nginx/conf.d/default.conf <<EOF
 
@@ -17,7 +18,7 @@ server {
 
     location / {
         proxy_pass ${BACKEND_URL};
-        proxy_set_header Host \$host;
+        proxy_set_header Host ${BACKEND_HOST};
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection \$connection_upgrade;
     }
